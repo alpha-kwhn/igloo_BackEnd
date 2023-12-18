c37dc82ab50f8f8241363b4a0282e12389c025a7
dlawjddn <jwl5015@naver.com>
2023-12-19 04:16:27 +0900

    Feat: result repository 생성

---
+++ igloo/src/main/java/donggukthon/team10/igloo/repository/ResultRepository.java
@@ -0,0 +1,7 @@
+package donggukthon.team10.igloo.repository;
+
+import donggukthon.team10.igloo.domain.Result;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultRepository extends JpaRepository<Result, Long> {
+}
