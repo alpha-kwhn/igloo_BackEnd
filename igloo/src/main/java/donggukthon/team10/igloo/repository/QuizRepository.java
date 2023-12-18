33b48700bb4c192b8ab463e3f5c36854ad6d282c
dlawjddn <jwl5015@naver.com>
2023-12-19 02:17:27 +0900

    Feat: 이글루의 퀴즈 조회 로직 생성

--- igloo/src/main/java/donggukthon/team10/igloo/repository/QuizRepository.java
+++ igloo/src/main/java/donggukthon/team10/igloo/repository/QuizRepository.java
@@ -1,7 +1,12 @@
 package donggukthon.team10.igloo.repository;
 
+import donggukthon.team10.igloo.domain.Igloo;
 import donggukthon.team10.igloo.domain.Quiz;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    List<Quiz> findAllByIgloo(Igloo igloo);
 }
