fb00cd40d440b369bbd933ccd3aac1ca8d35b2fd
Lim jeong woo <dlawjddn0105@gmail.com>
2023-12-18 14:27:43 +0900

    Add files via upload

---
+++ igloo/src/main/java/donggukthon/team10/igloo/IglooApplication.java
@@ -0,0 +1,13 @@
+package donggukthon.team10.igloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IglooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IglooApplication.class, args);
+	}
+
+}
---
+++ igloo/src/test/java/donggukthon/team10/igloo/IglooApplicationTests.java
@@ -0,0 +1,13 @@
+package donggukthon.team10.igloo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IglooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
