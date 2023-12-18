b1e537fcb990c97491259a8fd086b9c2e2555eb9
dlawjddn <jwl5015@naver.com>
2023-12-18 20:22:23 +0900

    Feat: error 추가

--- igloo/src/main/java/donggukthon/team10/igloo/exception/CustomErrorCode.java
+++ igloo/src/main/java/donggukthon/team10/igloo/exception/CustomErrorCode.java
@@ -5,20 +5,23 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum CustomErrorCode {
-    ;
+
     //400
 
 
     //401
-
+    NOT_LOGIN_USER(HttpStatus.UNAUTHORIZED, "NOT LOGIN USER"),
 
     //404
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "NOT FOUND USER CHECK YOUR INFO PLZ")
+
+    ;
 
     private final HttpStatus code;
     private final String errMessage;
 
-    CustomErrorCode(HttpStatus code, String errMesseage) {
+    CustomErrorCode(HttpStatus code, String errMessage) {
         this.code = code;
-        this.errMessage = errMesseage;
+        this.errMessage = errMessage;
     }
 }
