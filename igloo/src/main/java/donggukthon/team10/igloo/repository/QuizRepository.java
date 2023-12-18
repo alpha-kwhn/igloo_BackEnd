f6605d92520ff2d13a2fd559e5388fd48b2e3beb
dlawjddn <jwl5015@naver.com>
2023-12-19 04:15:10 +0900

    Fix: id 기준 오름차순 정렬 기능 추가

--- igloo/src/main/java/donggukthon/team10/igloo/repository/QuizRepository.java
+++ igloo/src/main/java/donggukthon/team10/igloo/repository/QuizRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    List<Quiz> findAllByIgloo(Igloo igloo);
+    List<Quiz> findAllByIglooOrderById(Igloo igloo);
 }
