111406ddc635316e368fe206120669ec852f8336
dlawjddn <jwl5015@naver.com>
2023-12-19 01:54:52 +0900

    Feat: quiz controller 생성 및 퀴즈 저장 api 완성

---
+++ igloo/src/main/java/donggukthon/team10/igloo/controller/QuizController.java
@@ -0,0 +1,27 @@
+package donggukthon.team10.igloo.controller;
+
+import donggukthon.team10.igloo.common.ApiResponse;
+import donggukthon.team10.igloo.dto.quiz.request.SaveQuizDTO;
+import donggukthon.team10.igloo.service.QuizService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/quiz")
+@RequiredArgsConstructor
+public class QuizController {
+    private final QuizService quizService;
+    @PostMapping("/{iglooId}")
+    public ApiResponse saveQuizzes(@PathVariable Long iglooId, @RequestBody List<SaveQuizDTO> quizzes){
+        quizService.saveQuizzes(iglooId, quizzes);
+        return ApiResponse.nullDataBuilder()
+                .code(HttpStatus.CREATED.value())
+                .message(HttpStatus.CREATED.getReasonPhrase())
+                .build();
+    }
+}
