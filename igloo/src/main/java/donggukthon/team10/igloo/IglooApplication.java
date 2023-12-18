2ab525dbf679ef663c1770e085ca4375f8ab79c2
dlawjddn <jwl5015@naver.com>
2023-12-19 01:51:10 +0900

    Feat: 한국 시간대로 설정

--- igloo/src/main/java/donggukthon/team10/igloo/IglooApplication.java
+++ igloo/src/main/java/donggukthon/team10/igloo/IglooApplication.java
@@ -1,11 +1,17 @@
 package donggukthon.team10.igloo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class IglooApplication {
-
+	@PostConstruct
+	public void start(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(IglooApplication.class, args);
 	}
