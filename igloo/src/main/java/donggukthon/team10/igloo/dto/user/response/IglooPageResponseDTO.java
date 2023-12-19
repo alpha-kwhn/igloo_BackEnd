1b8d546938e4b276b7dfe1f86707e767fd4d0092
dlawjddn <jwl5015@naver.com>
2023-12-19 23:21:04 +0900

    Feat: Igloo 화면 구성 response dto 생성

---
+++ igloo/src/main/java/donggukthon/team10/igloo/dto/user/response/IglooPageResponseDTO.java
@@ -0,0 +1,13 @@
+package donggukthon.team10.igloo.dto.user.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class IglooPageResponseDTO {
+    private String nickname;
+    private String code;
+    private String info;
+    private boolean owner;
+}
