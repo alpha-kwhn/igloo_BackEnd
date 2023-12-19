707776a209526ef752b0e6145fd68c80b4e80feb
김응관 <kwhn9486@naver.com>
2023-12-19 18:32:22 +0900

    롤링페이퍼 작성 api 구현

--- igloo/src/main/java/donggukthon/team10/igloo/domain/RollingPaper.java
+++ igloo/src/main/java/donggukthon/team10/igloo/domain/RollingPaper.java
@@ -1,6 +1,7 @@
 package donggukthon.team10.igloo.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -20,4 +21,12 @@ public class RollingPaper {
     private User writer;
     private String content;
     private int design;
+
+    @Builder
+    public RollingPaper(Igloo igloo, User writer, String content, int design) {
+        this.igloo = igloo;
+        this.writer = writer;
+        this.content = content;
+        this.design = design;
+    }
 }
