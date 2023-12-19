9f87864be98d99ba79a6139adc946b3eaf116911
dlawjddn <jwl5015@naver.com>
2023-12-20 02:08:20 +0900

    Feat: 내가 방문, 나를 방문에 대한 조회 jpql 로직 생성

--- igloo/src/main/java/donggukthon/team10/igloo/repository/VisitRepository.java
+++ igloo/src/main/java/donggukthon/team10/igloo/repository/VisitRepository.java
@@ -1,7 +1,18 @@
 package donggukthon.team10.igloo.repository;
 
+import donggukthon.team10.igloo.domain.Igloo;
 import donggukthon.team10.igloo.domain.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface VisitRepository extends JpaRepository<Visit, Long> {
+    @Query("SELECT DISTINCT v FROM Visit v WHERE v.visitor = :visitor ORDER BY v.visitedTime DESC")
+    List<Visit> findDistinctRecentVisitsByVisitor(@Param("visitor") Igloo visitor);
+    @Query("SELECT DISTINCT v FROM Visit v WHERE v.visited = :visited ORDER BY v.visitedTime DESC")
+    List<Visit> findDistinctRecentVisitsByVisited(@Param("visited") Igloo visited);
+
+
 }
