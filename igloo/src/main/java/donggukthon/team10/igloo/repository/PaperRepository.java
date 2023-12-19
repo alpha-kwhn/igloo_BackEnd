14dfd96ce64897a5845466757d2aa90b38483481
김응관 <kwhn9486@naver.com>
2023-12-19 23:27:58 +0900

    롤링페이퍼 관련 데이터를 다루기 위한 레포지토리계층 선언

--- igloo/src/main/java/donggukthon/team10/igloo/repository/PaperRepository.java
+++ igloo/src/main/java/donggukthon/team10/igloo/repository/PaperRepository.java
@@ -1,7 +1,21 @@
 package donggukthon.team10.igloo.repository;
 
+import donggukthon.team10.igloo.domain.Igloo;
 import donggukthon.team10.igloo.domain.RollingPaper;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PaperRepository extends JpaRepository<RollingPaper, Long> {
+    Optional<List<RollingPaper>> findAllRollingPapersByIgloo(Igloo igloo);
+
+    @Query("SELECT rp FROM RollingPaper rp WHERE rp.writer.id = :userId AND rp.igloo.id = :iglooId")
+    List<RollingPaper> findRollingPaperByWriterAndIgloo(@Param("iglooId") Long iglooId, @Param("userId") Long userId);
+
+    @Query("SELECT rp FROM RollingPaper rp WHERE rp.igloo.id = :iglooId AND rp.id = :paperId")
+    Optional<RollingPaper> findRollingPaperByIglooIdAndPaperId(@Param("iglooId") Long iglooId, @Param("paperId") Long paperId);
+
 }
