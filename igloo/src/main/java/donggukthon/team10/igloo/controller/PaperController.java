0f4a0e2eaab77dba5f4ee107fa3f771f45ff67e7
김응관 <kwhn9486@naver.com>
2023-12-19 23:26:39 +0900

    롤링페이퍼 관련 API 컨트롤러 계층 구현

--- igloo/src/main/java/donggukthon/team10/igloo/controller/PaperController.java
+++ igloo/src/main/java/donggukthon/team10/igloo/controller/PaperController.java
@@ -2,10 +2,17 @@ package donggukthon.team10.igloo.controller;
 
 import donggukthon.team10.igloo.common.ApiResponse;
 import donggukthon.team10.igloo.domain.RollingPaper;
+import donggukthon.team10.igloo.dto.paper.request.OpenDateDTO;
 import donggukthon.team10.igloo.dto.paper.request.WritePaperDTO;
+import donggukthon.team10.igloo.dto.paper.response.CreatedRollingPaperDTO;
+import donggukthon.team10.igloo.dto.paper.response.FindRollingPaperDTO;
+import donggukthon.team10.igloo.dto.paper.response.OpenedPaper;
 import donggukthon.team10.igloo.service.paper.PaperService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/paper")
@@ -15,12 +22,25 @@ public class PaperController {
     private final PaperService paperService;
 
     @GetMapping("/{iglooId}")
-    public ApiResponse<?> getRollingPapers(@PathVariable String iglooId) {
-        return null;
+    public ApiResponse<List<FindRollingPaperDTO>> getRollingPapers(@PathVariable("iglooId") Long iglooId) {
+        return ApiResponse.<List<FindRollingPaperDTO>>builder()
+                .code(HttpStatus.OK.value())
+                .message(HttpStatus.OK.getReasonPhrase())
+                .data(paperService.getRollingPapers(iglooId))
+                .build();
     }
 
     @PostMapping("/{iglooId}")
-    public ApiResponse<RollingPaper> writeRollingPapaer(@PathVariable Long iglooId, @RequestBody WritePaperDTO paper) {
+    public ApiResponse<CreatedRollingPaperDTO> writeRollingPaper(@PathVariable("iglooId") Long iglooId, @RequestBody WritePaperDTO paper) {
         return paperService.createRollingPaper(paper, iglooId);
     }
+
+    @GetMapping("/{iglooId}/")
+    public ApiResponse<OpenedPaper> openRollingPaper(@PathVariable("iglooId") Long iglooId,
+                                                     @RequestParam(name = "paperId") Long paperId,
+                                                     @RequestBody OpenDateDTO date) {
+        return paperService.openPaper(iglooId, paperId, date.date());
+    }
+
+
 }
