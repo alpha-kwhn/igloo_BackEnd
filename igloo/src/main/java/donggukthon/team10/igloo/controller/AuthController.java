a4a90899aed3b54b63af9ff2ad02d4675c41cda0
dlawjddn <jwl5015@naver.com>
2023-12-19 20:05:34 +0900

    Feat: login api 생성

--- igloo/src/main/java/donggukthon/team10/igloo/controller/AuthController.java
+++ igloo/src/main/java/donggukthon/team10/igloo/controller/AuthController.java
@@ -1,8 +1,11 @@
 package donggukthon.team10.igloo.controller;
 
 import donggukthon.team10.igloo.common.ApiResponse;
+import donggukthon.team10.igloo.dto.auth.request.LoginDTO;
 import donggukthon.team10.igloo.dto.user.request.SaveUserDTO;
 import donggukthon.team10.igloo.service.UserService;
+import donggukthon.team10.igloo.service.auth.LoginService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +18,9 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+    private final LoginService loginService;
+    private static final String BEARER_PREFIX = "Bearer ";
+
     @PostMapping("/register")
     public ApiResponse saveUser(@RequestBody SaveUserDTO saveUserDTO){
         userService.saveUser(saveUserDTO);
@@ -29,6 +35,14 @@ public class AuthController {
                 .code(HttpStatus.OK.value())
                 .message(HttpStatus.OK.getReasonPhrase())
                 .data(userService.isAvailableId(id))
+                .build();
+    }
+    @PostMapping("/login")
+    public ApiResponse login(@RequestBody LoginDTO loginDTO, HttpServletResponse response){
+        response.setHeader("Authorization", BEARER_PREFIX + loginService.login(loginDTO.getId(), loginDTO.getPassword()));
+        return ApiResponse.nullDataBuilder()
+                .code(HttpStatus.OK.value())
+                .message(HttpStatus.OK.getReasonPhrase())
                 .build();
     }
 }
