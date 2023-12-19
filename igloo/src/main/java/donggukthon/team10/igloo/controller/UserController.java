f3ea4ef58dabce98848d7b0fa62e39ee53a95f51
dlawjddn <jwl5015@naver.com>
2023-12-19 23:22:22 +0900

    Feat: 이글루 방문 api 완성

--- igloo/src/main/java/donggukthon/team10/igloo/controller/UserController.java
+++ igloo/src/main/java/donggukthon/team10/igloo/controller/UserController.java
@@ -1,8 +1,10 @@
 package donggukthon.team10.igloo.controller;
 
 import donggukthon.team10.igloo.common.ApiResponse;
+import donggukthon.team10.igloo.dto.user.request.IglooPageDTO;
 import donggukthon.team10.igloo.dto.user.request.UpdateInfoDTO;
 import donggukthon.team10.igloo.dto.user.request.UpdateNicknameDTO;
+import donggukthon.team10.igloo.service.IglooService;
 import donggukthon.team10.igloo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final IglooService iglooService;
     @PatchMapping("/nickname")
     public ApiResponse updateNickname(@RequestBody UpdateNicknameDTO updateNicknameDTO){
         return ApiResponse.builder()
@@ -22,11 +25,19 @@ public class UserController {
                 .build();
     }
     @PatchMapping("/info")
-    public  ApiResponse updateInfo(@RequestBody UpdateInfoDTO updateInfoDTO){
+    public ApiResponse updateInfo(@RequestBody UpdateInfoDTO updateInfoDTO){
         return ApiResponse.builder()
                 .code(HttpStatus.OK.value())
                 .message(HttpStatus.OK.getReasonPhrase())
                 .data(userService.updateInfo(updateInfoDTO))
+                .build();
+    }
+    @GetMapping("/mainPage")
+    public ApiResponse showIgloo(@RequestBody IglooPageDTO iglooPageDTO){
+        return ApiResponse.builder()
+                .code(HttpStatus.OK.value())
+                .message(HttpStatus.OK.getReasonPhrase())
+                .data(iglooService.showIglooInfo(iglooPageDTO.getCode()))
                 .build();
     }
 }
