6c22ab8c819dbd8e5d2800f8390c009c5170a394
김응관 <kwhn9486@naver.com>
2023-12-19 17:20:18 +0900

    id 값으로 유저를 찾는 함수 추가

--- igloo/src/main/java/donggukthon/team10/igloo/repository/UserRepository.java
+++ igloo/src/main/java/donggukthon/team10/igloo/repository/UserRepository.java
@@ -3,5 +3,8 @@ package donggukthon.team10.igloo.repository;
 import donggukthon.team10.igloo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserById(Long id);
 }
